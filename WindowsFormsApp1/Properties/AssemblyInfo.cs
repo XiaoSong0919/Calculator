4978dc7c3539c3c8d01f7945c84a5348bd48ece2
CannonCart <42860838+XiaoSong0919@users.noreply.github.com>
2020-04-06 14:31:18 +0800

    Add files via upload

---
+++ WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("计算器")]
+[assembly: AssemblyDescription("一个基于C#编写的简易轻便的计算器")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Person of CannonCart")]
+[assembly: AssemblyProduct("计算器（基于C#）")]
+[assembly: AssemblyCopyright("Copyright ©  2019-2020 宋宇皓|代码所有权归宋宇皓所有")]
+[assembly: AssemblyTrademark("CannonCart")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("b9491917-b8ce-44de-ac3f-0deecb64b7cc")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+// 方法是按如下所示使用“*”: :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
