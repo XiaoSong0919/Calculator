d20a8a54b3972f0a0b778b56efec7727ae97717e
yuhao song <1499602914@qq.com>
2020-11-05 22:29:20 +0800

    fix bug_uploads

--- WindowsFormsApp1/Form5.cs
+++ WindowsFormsApp1/Form5.cs
@@ -52,52 +52,66 @@ namespace WindowsFormsApp1
             string strM = DateTime.Now.Minute.ToString();
             string text = textBox1.Text + "\r\n" + textBox2.Text + "\r\n" + textBox3.Text;
             newtxt(text, DateTime.Now.Year.ToString() + "-" + DateTime.Now.DayOfYear.ToString() + "-"  + strY + "-" + strM + "-" + "bug_upload.txt");
-            Upload("ftp","", DateTime.Now.Year.ToString() + "-" + DateTime.Now.DayOfYear.ToString() + "-" + strY + "-" + strM + "-" + "bug_upload.txt", "ftp://songyuhao.51vip.biz:24/bugs_upload/");
+            Upload("ftp","", DateTime.Now.Year.ToString() + "-" + DateTime.Now.DayOfYear.ToString() + "-" + strY + "-" + strM + "-" + "bug_upload.txt", "ftp://nas.cannon.org.cn:24/bugs_upload/");
         }
         
         public void Upload(string userId, string pwd, string filename, string ftpPath)
         {
-            Form1 f1 = new Form1();
-            FileInfo fileInf = new FileInfo(f1.path + "/Calculator/" + filename);
-            FtpWebRequest reqFTP;
-            // 根据uri创建FtpWebRequest对象
-            reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(ftpPath + fileInf.Name));
-            // ftp用户名和密码
-            reqFTP.Credentials = new NetworkCredential(userId, pwd);
-
-            reqFTP.KeepAlive = false;
-            reqFTP.Method = WebRequestMethods.Ftp.UploadFile;
-            reqFTP.UseBinary = true;
-            reqFTP.UsePassive = true;//被动模式(这个要和搭建的ftp服务对应)
-            reqFTP.ContentLength = fileInf.Length;
-            reqFTP.Timeout = 1000 * 60 * 5;//响应请求等待的时间
-            // 缓冲大小设置为2kb
-            int buffLength = 2048;
-            byte[] buff = new byte[buffLength];
-            int contentLen;
-            // 打开一个文件流 (System.IO.FileStream) 去读上传的文件
-            FileStream fs = fileInf.OpenRead();
-            //try
-            //{
-            // 把上传的文件写入流
-            using (Stream strm = reqFTP.GetRequestStream())
+            try
             {
-                contentLen = fs.Read(buff, 0, buffLength);
-                while (contentLen != 0)
+                Form1 f1 = new Form1();
+                FileInfo fileInf = new FileInfo(f1.path + "/Calculator/" + filename);
+                FtpWebRequest reqFTP;
+                // 根据uri创建FtpWebRequest对象
+                reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(ftpPath + fileInf.Name));
+                // ftp用户名和密码
+                reqFTP.Credentials = new NetworkCredential(userId, pwd);
+
+                reqFTP.KeepAlive = false;
+                reqFTP.Method = WebRequestMethods.Ftp.UploadFile;
+                reqFTP.UseBinary = true;
+                reqFTP.UsePassive = true;//被动模式(这个要和搭建的ftp服务对应)
+                reqFTP.ContentLength = fileInf.Length;
+                reqFTP.Timeout = 2000;//响应请求等待的时间
+                                               // 缓冲大小设置为2kb
+                int buffLength = 2048;
+                byte[] buff = new byte[buffLength];
+                int contentLen;
+                // 打开一个文件流 (System.IO.FileStream) 去读上传的文件
+                FileStream fs = fileInf.OpenRead();
+                //try
+                //{
+                // 把上传的文件写入流
+                using (Stream strm = reqFTP.GetRequestStream())
                 {
-                    strm.Write(buff, 0, contentLen);
                     contentLen = fs.Read(buff, 0, buffLength);
+                    while (contentLen != 0)
+                    {
+                        strm.Write(buff, 0, contentLen);
+                        contentLen = fs.Read(buff, 0, buffLength);
+                    }
+                    // 关闭两个流
+                    strm.Close();
+                    fs.Close();
                 }
-                // 关闭两个流
-                strm.Close();
-                fs.Close();
+                MessageBox.Show("反馈成功！感谢您的支持！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            // }
+            catch (WebException e)
+            {
+                MessageBox.Show(e.ToString(), "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //throw e;
+            }
+            catch (InvalidOperationException e)
+            {
+                MessageBox.Show("请关闭代理工具！","Error!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+            
             // catch (Exception ex)
             // {
 
             // }
-            MessageBox.Show("反馈成功！感谢您的支持！","成功",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            
         
 
 
